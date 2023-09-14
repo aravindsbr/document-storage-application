d6cce6e941e2fffaed4c8f31d95fbed0b5620d1d
aravinds <aravindsbr@gmail.com>
2023-09-14 18:07:12 +0530

    file upload ui added

--- document-storage-application/src/app/upload/upload.component.ts
+++ document-storage-application/src/app/upload/upload.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.scss']
+  styleUrls: ['./upload.component.scss'],
 })
 export class UploadComponent {
+  fileDetails: any;
 
+  setFileInfo(event: any) {
+    this.fileDetails = event.target.files[0];
+  }
+
+  handleFileUpload() {
+    if (this.fileDetails) {
+      console.log('details are', this.fileDetails);
+    }
+  }
 }
